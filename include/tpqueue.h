2b30b4a32de3ba2f9048e83d16f0870f555d0798
Andrey-Trubachnov <137098778+Andrey-Trubachnov@users.noreply.github.com>
2025-04-22 20:57:58 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,51 +11,53 @@ class TPQueue {
   TPQueue() : head(nullptr), tail(nullptr) {}
 
   bool isEmpty() {
-   return head == nullptr;
+    return head == nullptr;
   }
-  
+
   void push(T&& item) {
-   if (isEmpty()) {
-     head = tail = &item;
-     return;
-   }
-  
-   T* temp = head;
-   while (temp != nullptr && temp->prior >= (&item)->prior) {
-     temp = temp->next;
-   }
-  
-   if (temp == nullptr) {
-     tail->next = &item;
-     (&item)->prev = tail;
-     tail = (&item);
-   } else if (temp == head) {
-     (&item)->next = head;
-     head->prev = (&item);
-     head = (&item);
-   } else {
-     (&item)->next = temp;
-     (&item)->prev = temp->prev;
-     temp->prev->next = (&item);
-     temp->prev = (&item);
-   }
+    if (isEmpty()) {
+      head = tail = &item;
+      return;
+    }
+
+    T* temp = head;
+    while (temp != nullptr && temp->prior >= (&item)->prior) {
+      temp = temp->next;
+    }
+
+    if (temp == nullptr) {
+      tail->next = &item;
+      (&item)->prev = tail;
+      tail = (&item);
+    }
+    else if (temp == head) {
+      (&item)->next = head;
+      head->prev = (&item);
+      head = (&item);
+    }
+    else {
+      (&item)->next = temp;
+      (&item)->prev = temp->prev;
+      temp->prev->next = (&item);
+      temp->prev = (&item);
+    }
   }
-  
+
   T pop() {
-   if (isEmpty()) {
-     throw("Список пуст");
-   }
-  
-   T* t = head;
-  
-   if (head == tail) {
-     head = tail = nullptr;
-     return *t;
-   }
-  
-   head = head->next;
-   head->prev = nullptr;
-   return *t;
+    if (isEmpty()) {
+      throw("Список пуст");
+    }
+
+    T* t = head;
+
+    if (head == tail) {
+      head = tail = nullptr;
+      return *t;
+    }
+
+    head = head->next;
+    head->prev = nullptr;
+    return *t;
   }
 };
 
