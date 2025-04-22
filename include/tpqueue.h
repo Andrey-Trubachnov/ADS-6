b3e34aea76149d43e7ba023e7ca40003c319fe11
Andrey-Trubachnov <137098778+Andrey-Trubachnov@users.noreply.github.com>
2025-04-22 20:34:22 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,66 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  T* head;
+  T* tail;
+
+ public:
+   TPQueue() : head(nullptr), tail(nullptr) {}
+
+   bool isEmpty() {
+     return head == nullptr;
+   }
+
+   void push(T&& item) {
+     if (isEmpty()) {
+       head = tail = &item;
+       return;
+     }
+
+     T* temp = head;
+     while (temp->prior >= (&item)->prior) {
+       temp = temp->next;
+     }
+
+     if (temp == nullptr) {
+       tail->next = &item;
+       (&item)->prev = tail;
+       tail = (&item);
+     } else if (temp == head) {
+       (&item)->next = head;
+       head->prev = (&item);
+       head = (&item);
+     } else {
+       (&item)->next = temp;
+       (&item)->prev = temp->prev;
+       temp->prev->next = (&item);
+       temp->prev = (&item);
+     }
+   }
+
+   T pop() {
+     if (isEmpty()) {
+       throw("Список пуст");
+     }
+
+     T* t = head;
+
+     if (head == tail) {
+       head = tail = nullptr;
+       return *t;
+     }
+
+     head = head->next;
+     head->prev = nullptr;
+     return *t;
+   }
 };
 
 struct SYM {
   char ch;
   int prior;
+  SYM* next = nullptr;
+  SYM* prev = nullptr;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
